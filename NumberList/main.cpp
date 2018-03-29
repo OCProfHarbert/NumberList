bf80450f84b07509c94fb31afb6760abde94fb18
andy.harbert <andy.harbert@oc.edu>
2018-03-29 09:15:39 -0500

    Add project files.

---
+++ NumberList/main.cpp
@@ -0,0 +1,16 @@
+#include "NumberList.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// replace all of the following code; this is just test
+	// code from the book
+	NumberList list;
+
+	list.appendNode(10);
+	list.appendNode(20);
+	list.appendNode(30);
+	list.appendNode(40);
+	list.displayList();
+}
